548b82ceb962d1be6a2fd99c374eeffd183d08ee
Christiaan Bloemendaal <github@tnrd.net>
2024-05-18 16:52:09 +0200

    Api things

---
+++ src/Indexarr.Api.V1/Chapters/ChapterController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Core.Chapters;
+using NzbDrone.Core.IndexedMangas;
+using NzbDrone.Core.Mangas;
+using Prowlarr.Http;
+using Prowlarr.Http.REST;
+
+namespace Prowlarr.Api.V1.Chapters;
+
+[V1ApiController]
+public class ChapterController : RestController<ChapterResource>
+{
+    private readonly IChapterService _chapterService;
+    private readonly IIndexedMangaService _indexedMangaService;
+    private readonly IMangaService _mangaService;
+
+    public ChapterController(IChapterService chapterService,
+        IIndexedMangaService indexedMangaService,
+        IMangaService mangaService)
+    {
+        _chapterService = chapterService;
+        _indexedMangaService = indexedMangaService;
+        _mangaService = mangaService;
+    }
+
+    public override ChapterResource GetResourceById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("mangaupdates")]
+    [Produces("application/json")]
+    public List<ChapterResource> GetByMangaUpdatesId(long mangaUpdatesId)
+    {
+        var manga = _mangaService.GetByMangaUpdatesId(mangaUpdatesId);
+        if (manga == null)
+        {
+            return new List<ChapterResource>();
+        }
+
+        var resources = new List<ChapterResource>();
+        var indexedMangas = _indexedMangaService.GetByMangaId(manga.Id);
+
+        foreach (var indexedManga in indexedMangas)
+        {
+            var chapters = _chapterService.GetForIndexedManga(indexedManga.Id);
+            resources.AddRange(chapters.Select(c => c.ToResource()));
+        }
+
+        return resources;
+    }
+}
---
+++ src/Indexarr.Api.V1/Chapters/ChapterResource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NzbDrone.Core.Chapters;
+using Prowlarr.Http.REST;
+
+namespace Prowlarr.Api.V1.Chapters;
+
+public class ChapterResource : RestResource
+{
+    public int Volume { get; set; }
+    public decimal Number { get; set; }
+    public DateTime Date { get; set; }
+}
+
+public static class ChapterResourceMapper
+{
+    public static ChapterResource ToResource(this Chapter model)
+    {
+        return new ChapterResource
+        {
+            Id = model.Id,
+            Volume = model.Volume,
+            Number = model.Number,
+            Date = model.Date
+        };
+    }
+}
