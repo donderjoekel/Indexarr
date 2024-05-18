99419247e291e261b700fa4ffa48ef4e85eafba2
Christiaan Bloemendaal <github@tnrd.net>
2024-05-18 16:50:24 +0200

    Add volume to chapter

--- src/NzbDrone.Core/Parser/Model/ChapterInfo.cs
+++ src/NzbDrone.Core/Parser/Model/ChapterInfo.cs
@@ -4,6 +4,7 @@ namespace NzbDrone.Core.Parser.Model;
 
 public class ChapterInfo
 {
+    public int Volume { get; set; }
     public decimal Number { get; set; }
     public string Url { get; set; }
     public DateTime Date { get; set; }
