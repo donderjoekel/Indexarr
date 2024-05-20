c868f237ae5fa21788c7467c5f3e309403f52c15
Christiaan Bloemendaal <github@tnrd.net>
2024-05-20 09:00:18 +0200

    Update MangaUpdates implementation

--- src/NzbDrone.Core/Metadata/MangaUpdates/MangaUpdatesService.cs
+++ src/NzbDrone.Core/Metadata/MangaUpdates/MangaUpdatesService.cs
@@ -2,18 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 using NLog;
 using NzbDrone.Common.Cloud;
 using NzbDrone.Common.Extensions;
 using NzbDrone.Common.Http;
 using NzbDrone.Common.Serializer;
-using NzbDrone.Core.Http.CloudFlare;
-using NzbDrone.Core.Indexers;
 using NzbDrone.Core.MetadataSource.MangaUpdates.Resource.SeriesSearch;
 using NzbDrone.Core.MetadataSource.MangaUpdates.Result;
-using Polly;
-using Polly.Retry;
 
 namespace NzbDrone.Core.Metadata.MangaUpdates;
 
@@ -23,57 +18,17 @@ public interface IMangaUpdatesService
     IEnumerable<string> GetTitles(long mangaUpdatesId);
 }
 
-public class MangaUpdatesService : IMangaUpdatesService
+public class MangaUpdatesService : MetadataSource, IMangaUpdatesService
 {
-    private readonly IHttpClient _httpClient;
     private readonly Logger _logger;
     private readonly IHttpRequestBuilderFactory _requestBuilder;
-
-    private ResiliencePipeline<HttpResponse> RetryStrategy => new ResiliencePipelineBuilder<HttpResponse>()
-        .AddRetry(new RetryStrategyOptions<HttpResponse>
-        {
-            ShouldHandle = static args => args.Outcome switch
-            {
-                { Result.HasHttpServerError: true } => PredicateResult.True(),
-                { Result.StatusCode: HttpStatusCode.RequestTimeout } => PredicateResult.True(),
-                { Exception: HttpException { Response.HasHttpServerError: true } } => PredicateResult.True(),
-                _ => PredicateResult.False()
-            },
-            Delay = TimeSpan.FromSeconds(1),
-            MaxRetryAttempts = 2,
-            BackoffType = DelayBackoffType.Exponential,
-            UseJitter = true,
-            OnRetry = args =>
-            {
-                var exception = args.Outcome.Exception;
-
-                if (exception is not null)
-                {
-                    _logger.Warn(exception,
-                        "Request for '{0}' failed with exception '{1}'. Retrying in {2}s.",
-                        args.Outcome.Result?.Request.Url.ToString() ?? "MangaUpdatesService",
-                        exception.Message,
-                        args.RetryDelay.TotalSeconds);
-                }
-                else
-                {
-                    _logger.Warn("Request for '{0}' failed with status {1}. Retrying in {2}s.",
-                        args.Outcome.Result?.Request.Url.ToString() ?? "MangaUpdatesService",
-                        args.Outcome.Result?.StatusCode,
-                        args.RetryDelay.TotalSeconds);
-                }
-
-                return default;
-            }
-        })
-        .Build();
 
     public MangaUpdatesService(
         IHttpClient httpClient,
         Logger logger,
         IMangaUpdatesCloudRequestBuilder requestBuilder)
+        : base(httpClient, logger)
     {
-        _httpClient = httpClient;
         _logger = logger;
         _requestBuilder = requestBuilder.Services;
     }
@@ -203,62 +158,5 @@ public class MangaUpdatesService : IMangaUpdatesService
         }
 
         return true;
-    }
-
-    private async Task<Response> ExecuteRequestAsync(HttpRequest request)
-    {
-        _logger.Debug("Downloading Feed " + request.ToString(false));
-
-        // TODO: possibly disable this
-        request.LogResponseContent = true;
-
-        var originalUrl = request.Url;
-
-        /*Cookies = GetCookies();
-
-        if (Cookies != null)
-        {
-            foreach (var cookie in Cookies)
-            {
-                request.Cookies.Add(cookie.Key, cookie.Value);
-            }
-        }*/
-
-        request.SuppressHttpError = true;
-
-        var response = await RetryStrategy
-            .ExecuteAsync(
-                static async (state, _) => await state._httpClient.ExecuteAsync(state.request),
-                (_httpClient, request))
-            .ConfigureAwait(false);
-
-        if (CloudFlareDetectionService.IsCloudflareProtected(response))
-        {
-            throw new CloudFlareProtectionException(response);
-        }
-
-        // Throw common http errors here before we try to parse
-        if (response.HasHttpError && (request.SuppressHttpErrorStatusCodes == null ||
-                                      !request.SuppressHttpErrorStatusCodes.Contains(response.StatusCode)))
-        {
-            if (response.Request.LogHttpError)
-            {
-                _logger.Warn("HTTP Error - {0}", response);
-            }
-
-            if (response.StatusCode == HttpStatusCode.TooManyRequests)
-            {
-                throw new TooManyRequestsException(request, response);
-            }
-
-            if (response.HasHttpServerError)
-            {
-                throw new HttpException(request, response);
-            }
-        }
-
-        /*UpdateCookies(request.Cookies, DateTime.Now.AddDays(30));*/
-
-        return new Response(request, response);
     }
 }
