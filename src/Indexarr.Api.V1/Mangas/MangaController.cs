cf085ab9fe0fb5d566cdf599042b90c8f5a0e8e9
Christiaan Bloemendaal <github@tnrd.net>
2024-05-28 19:37:01 +0200

    Add basic MangaController and MangaResource

---
+++ src/Indexarr.Api.V1/Mangas/MangaController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.Mangas;
+using Prowlarr.Http;
+using Prowlarr.Http.REST;
+
+namespace Prowlarr.Api.V1.Mangas;
+
+[V1ApiController]
+public class MangaController : RestController<MangaResource>
+{
+    private readonly IMangaService _mangaService;
+
+    public MangaController(IMangaService mangaService)
+    {
+        _mangaService = mangaService;
+    }
+
+    public override MangaResource GetResourceById(int id)
+    {
+        return _mangaService.Find(id).ToResource();
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    [Produces("application/json")]
+    public List<MangaResource> Get()
+    {
+        return _mangaService.All().SelectList(x => x.ToResource());
+    }
+}
