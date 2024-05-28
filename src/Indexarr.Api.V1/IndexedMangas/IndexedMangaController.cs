1099605de075a01243e4ff26abd02d7f5b6d4ebb
Christiaan Bloemendaal <github@tnrd.net>
2024-05-28 14:08:11 -0400

    feat: indexed manga api (#15)
    * Expose methods in service
    
    * Add IndexedMangaController
    
    * Formatting
    
    * Fix time parsing

---
+++ src/Indexarr.Api.V1/IndexedMangas/IndexedMangaController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NzbDrone.Common.Extensions;
+using NzbDrone.Core.IndexedMangas;
+using Prowlarr.Http;
+using Prowlarr.Http.REST;
+
+namespace Prowlarr.Api.V1.IndexedMangas;
+
+[V1ApiController]
+public class IndexedMangaController : RestController<IndexedMangaResource>
+{
+    private readonly IIndexedMangaService _indexedMangaService;
+
+    public IndexedMangaController(IIndexedMangaService indexedMangaService)
+    {
+        _indexedMangaService = indexedMangaService;
+    }
+
+    public override IndexedMangaResource GetResourceById(int id)
+    {
+        return _indexedMangaService.Find(id).ToResource();
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    [Produces("application/json")]
+    public List<IndexedMangaResource> Get()
+    {
+        return _indexedMangaService.All().SelectList(x => x.ToResource());
+    }
+}
