8c79df7160c7d180b4888ad90e71e57bee783d51
Christiaan Bloemendaal <github@tnrd.net>
2024-05-28 20:07:28 +0200

    Add IndexedMangaController

---
+++ src/Indexarr.Api.V1/IndexedMangas/IndexedMangaResource.cs
@@ -0,0 +1,27 @@
+﻿using NzbDrone.Core.IndexedMangas;
+using Prowlarr.Http.REST;
+
+namespace Prowlarr.Api.V1.IndexedMangas;
+
+public class IndexedMangaResource : RestResource
+{
+    public int? MangaId { get; set; }
+    public int IndexerId { get; set; }
+    public string Title { get; set; }
+    public string Url { get; set; }
+}
+
+public static class IndexedMangaResourceMapper
+{
+    public static IndexedMangaResource ToResource(this IndexedManga model)
+    {
+        return new IndexedMangaResource()
+        {
+            Id = model.Id,
+            MangaId = model.MangaId,
+            IndexerId = model.IndexerId,
+            Title = model.Title,
+            Url = model.Url
+        };
+    }
+}
