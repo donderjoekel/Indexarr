b8a86bb84aab5f58f8d75c85e7047dcd7d9c212c
Christiaan Bloemendaal <github@tnrd.net>
2024-05-30 19:42:20 +0200

    Add request builders

---
+++ src/NzbDrone.Core/Drones/DirectorRequestBuilder.cs
@@ -0,0 +1,44 @@
+﻿using NzbDrone.Common.Http;
+using NzbDrone.Core.Configuration;
+using NzbDrone.Core.Configuration.Events;
+using NzbDrone.Core.Messaging.Events;
+
+namespace NzbDrone.Core.Drones;
+
+public interface IDirectorRequestBuilder
+{
+    HttpRequestBuilder Builder { get; }
+}
+
+public class DirectorRequestBuilder : IDirectorRequestBuilder,
+    IHandle<ConfigFileSavedEvent>
+{
+    private readonly IConfigFileProvider _configFile;
+
+    private IHttpRequestBuilderFactory _factory;
+
+    public DirectorRequestBuilder(IConfigFileProvider configFile)
+    {
+        _configFile = configFile;
+        UpdateFactory();
+    }
+
+
+    public HttpRequestBuilder Builder => _factory.Create();
+
+    public void Handle(ConfigFileSavedEvent message)
+    {
+        UpdateFactory();
+    }
+
+    private void UpdateFactory()
+    {
+        if (_configFile.IsDirector)
+        {
+            _factory = new HttpRequestBuilder(_configFile.DirectorAddress)
+                .Resource("/api/v1/drone")
+                .SetHeader("X-Api-Key", _configFile.ApiKey)
+                .CreateFactory();
+        }
+    }
+}
---
+++ src/NzbDrone.Core/Drones/DroneRequestBuilder.cs
@@ -0,0 +1,28 @@
+﻿using NzbDrone.Common.Http;
+using NzbDrone.Core.Configuration;
+
+namespace NzbDrone.Core.Drones;
+
+public interface IDroneRequestBuilder
+{
+    HttpRequestBuilder Create(Drone drone);
+}
+
+public class DroneRequestBuilder : IDroneRequestBuilder
+{
+    private readonly IConfigFileProvider _configFile;
+
+    public DroneRequestBuilder(IConfigFileProvider configFile)
+    {
+        _configFile = configFile;
+    }
+
+    public HttpRequestBuilder Create(Drone drone)
+    {
+        return new HttpRequestBuilder(drone.Address)
+            .Resource("/api/v1/drone")
+            .SetHeader("X-Api-Key", _configFile.ApiKey)
+            .CreateFactory()
+            .Create();
+    }
+}
