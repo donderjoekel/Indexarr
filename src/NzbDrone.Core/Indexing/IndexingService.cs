1b86c3fbfef313df1cc3f0396f2509e401d59a14
Christiaan Bloemendaal <github@tnrd.net>
2024-05-30 11:55:57 +0200

    Limit amount of concurrent work

--- src/NzbDrone.Core/Indexing/IndexingService.cs
+++ src/NzbDrone.Core/Indexing/IndexingService.cs
@@ -92,7 +92,7 @@ public class IndexingService : IIndexingService,
         var indexer = _indexerFactory.GetByGuid(message.IndexerId);
         _logger.Info("Starting full index for {Indexer}", indexer.Name);
         var result = indexer.FullIndex().GetAwaiter().GetResult();
-        ConcurrentWork.CreateAndRun(5, result.Mangas, info => () => ProcessManga(info));
+        ConcurrentWork.CreateAndRun(1, result.Mangas, info => () => ProcessManga(info));
         _logger.Info("Finished full index for {Indexer}", indexer.Name);
         _droneService.DispatchPartialIndexFinished(message.IndexerId);
     }
